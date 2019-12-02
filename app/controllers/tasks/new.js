9c89218e2dc5f9a7ea76a991a1014e0f6bad9cd2
heretodesign <senwelo.works@gmail.com>
2019-12-02 16:43:19 +0800

    added tasks controllers and routes

--- app/controllers/tasks/new.js
+++ app/controllers/tasks/new.js
@@ -1,4 +1,14 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    actions: {
+        addTask: function() {
+            let title = this.get('title');
+            let date = this.get('date');
+            let description = this.get('description');
+            
+            //create new task...
+            let newTask = this.transitionToRoute.createRecord();
+        }
+    }
 });
